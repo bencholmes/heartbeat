311c1274b06cca6f9cccf5e85931c6f02fd2a166
Ben Holmes <bholmes02@qub.ac.uk>
2016-10-01 18:15:39 +0100

    Added audio examples and re-commented plot section.

--- README.md
+++ README.md
@@ -6,6 +6,12 @@ Create realistic sounding heartbeats suitable for use in action/thriller/horror 
 
 Image of a model heart is under public domain, CC0 license.
 
+## Examples
+
+[Heartbeat at 60bpm](/media/heartbeat-60bpm.wav)
+
+[Heartbeat at 140bpm](/media/heartbeat-60bpm.wav)
+
 ## Install
 
 Download the repository and copy it to your MATLAB working directory. Voila!
@@ -37,7 +43,7 @@ There is a commented section, *Plot and Listen*, within the script. Uncommenting
 
 Examining the plot will show two waveforms: the first is the unprocessed heartbeat, the second the heartbeat with additional filtering. The additional filtering is added to recreate the sound of the chest cavity, to model a sound such as when a doctor is listening to your heart. The filters used are effectively two resonant filters, with their centre frequency adjusted relative to the tempo of the heartbeat.
 
-        % 3rd order butterworth bandpass filter with
+    % 3rd order butterworth bandpass filter with
     % corner frequencies at 20 and 140 Hz.
     butter(3,[20 140+tempo]/(0.5*fs),'bandpass')
 
@@ -51,13 +57,13 @@ Changing the values in these lines will have the largest effect on the sound of 
 
 The unprocessed waveform is synthesised from multiple consecutive Hann windows (bell shaped lines). These are loosely based on an EKG, with sections: P, Q, R, etc. There are two parameters controlling each section, the length and the amplitude.
 
-        % Length of the p section, with a length of 1/9
-        % of the total length.
-        p_length = floor((0.75 + rand()/2) * pulse_Ns/9);
+    % Length of the p section, with a length of 1/9
+    % of the total length.
+    p_length = floor((0.75 + rand()/2) * pulse_Ns/9);
 
-        % Amplitude of the the p section, with a gain of
-        % 0.1.
-        p_amp = (0.75 + rand()/2) * 0.1;
+    % Amplitude of the the p section, with a gain of
+    % 0.1.
+    p_amp = (0.75 + rand()/2) * 0.1;
 
 The lengths are given as a factor of the total length `pulse_Ns`. The amplitude is controlled with an offset, a random scaled number, and a scaling factor. The random number from `rand()` prevents the heartbeats sounding too similar, and will always fall between 0 and 1. By adding 0.75 to this, the section will never be silent. The scaling factor at the end of the expression is the value that will give the best results when changed.
 
--- heartbeat.m
+++ heartbeat.m
@@ -35,54 +35,54 @@ for n = 1:num_beats*2
     p_length = floor((0.75 + rand()/2)*pulse_Ns/9);
     p_amp = (0.75 + rand()/2)*0.1;
     P = p_amp*hann(p_length).';
-    
+
     % PR segment
     pr_length = floor((0.75 + rand()/2)*pulse_Ns/8);
     PR = zeros(1,pr_length);
-    
+
     % Q
     q_length = floor((0.75 + rand()/2)*pulse_Ns/24);
     q_amp = (0.75 + rand()/2)*0.1;
     Q = -q_amp*hann(q_length).';
-    
+
     % R
     r_length = floor((0.75 + rand()/2)*pulse_Ns/6);
     r_amp = (0.75 + rand()/2)*1;
     R = r_amp*hann(r_length).';
-    
+
     % S
     s_length = floor((0.75 + rand()/2)*pulse_Ns/24);
     s_amp = (0.75 + rand()/2)*0.3;
     S = -s_amp*hann(s_length).';
-    
+
     % ST segment
     st_length = floor((0.75 + rand()/2)*pulse_Ns/9);
     ST = zeros(1,st_length);
-    
+
     % T
     t_length = floor((0.75 + rand()/2)*pulse_Ns/9);
     t_amp = (0.75 + rand()/2)*0.2;
     T = t_amp*hann(t_length).';
-    
+
     % U
     u_length = floor((0.75 + rand()/2)*pulse_Ns/11);
     u_amp = (0.75 + rand()/2)*0.1;
     U = u_amp*hann(u_length).';
-    
+
     % Find the total number of samples left after adding together all
     % sections.
     sum_length = p_length + pr_length + q_length + r_length + s_length +...
         st_length + t_length + u_length;
-    
+
     % Remaining number of samples.
     rem_ns = pulse_Ns - sum_length;
-    
+
     % Silence for the remaining number of samples.
     rem = zeros(1,rem_ns);
-    
+
     %% Stitch together
     single_pulse = [ P PR  Q R S ST T U rem ];
-    
+
     % A condition to switch between two different lengths between pulses,
     % and apply a gain difference.
     if mod(n,2)
@@ -94,10 +94,10 @@ for n = 1:num_beats*2
         inter_pulse = zeros(1, floor(3*(((total_dur)*fs) - 2*pulse_Ns)/4) );
         gain = 1;
     end
-    
+
     % Stick the pulses together (admittedly in an ugly method).
     pulse = [pulse gain.*[single_pulse inter_pulse]];
-    
+
 end
 
 %% Add resonant "abdomen"
@@ -112,12 +112,12 @@ filtered_pulse = filter(b_but,a_but,pulse);
 filtered_pulse = filter(b_peak, a_peak, filtered_pulse);
 
 %% Plot and Listen
-fig=figure(1);
-clf;
-plot(t, filtered_pulse,t,pulse);
-xlabel('Time (s)'); ylabel('Amplitude'); 
-legend('Filtered Pulse','Unfiltered Pulse'); title('Heartbeat Waveforms');
-soundsc(filtered_pulse,fs);
+% fig=figure(1);
+% clf;
+% plot(t, filtered_pulse,t,pulse);
+% xlabel('Time (s)'); ylabel('Amplitude');
+% legend('Filtered Pulse','Unfiltered Pulse'); title('Heartbeat Waveforms');
+% soundsc(filtered_pulse,fs);
 
 %% Export
 
